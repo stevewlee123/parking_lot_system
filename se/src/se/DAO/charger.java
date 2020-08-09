ccd2195ab597dc7542a0758a585ca2d806e87c16
QINZHOU LI <55862308+stevewlee123@users.noreply.github.com>
2020-08-09 15:14:17 -0400

    Add files via upload

---
+++ se/src/se/DAO/charger.java
@@ -0,0 +1,29 @@
+
+
+package se.DAO;
+
+public class charger {
+	private String cardtype;
+	private String stationtype;
+	private int charge;
+	public String getCardtype() {
+		return cardtype;
+	}
+	public void setCardtype(String cardtype) {
+		this.cardtype = cardtype;
+	}
+	public String getStationtype() {
+		return stationtype;
+	}
+	public void setStationtype(String stationtype) {
+		this.stationtype = stationtype;
+	}
+	public int getCharge() {
+		return charge;
+	}
+	public void setCharge(int charge) {
+		this.charge = charge;
+	}
+	
+
+}
