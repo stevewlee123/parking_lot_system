6034b10692825d522ad45d574205f9bb42d87bd4
QINZHOU LI <55862308+stevewlee123@users.noreply.github.com>
2020-08-12 22:51:22 -0400

    Add files via upload

---
+++ parkSys/src/parkSys/Objects/sit_infor.java
@@ -0,0 +1,22 @@
+
+package se.DAO;
+
+public class sit_infor {
+	private int stationid;
+	private String stationtype;
+	public int getStationid() {
+		return stationid;
+	}
+	public void setStationid(int stationid) {
+		this.stationid = stationid;
+	}
+	public String getStationtype() {
+		return stationtype;
+	}
+	public void setStationtype(String stationtype) {
+		this.stationtype = stationtype;
+	}
+	
+	
+
+}
